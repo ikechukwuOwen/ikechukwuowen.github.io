484cfec69443f2589e070b5a30aeb43fceaa02b7
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-16 12:13:26 +0000

    hot

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-var t = 20;
-var u = 10;
+var t = 20 > 10;
+var u = 70 < 10;
 
-consolelog(t + u);
+console.log ( t ); 
+console.log ( u );
+
+var a = "bole";
+var b = "hed";
+var c = " ";
+ 
+console.log( a + c + b);
+
+var sum = function(x, y){
+    return x + y;
+}
+
+var fourplustwo = sum(4, 2);
+console.log(fourplustwo);
+
+var crazy = function(a, b, c){
+    return (a - b) + c;
+}
+
+var tom = crazy( 15, 9, 4);
+console.log(tom);
+
+var weather = function(temp){
+    console.log("The temprature is", temp, "degrees");
+
+if(temp <= 70) {
+    console.log("frezzing don't com outside");
+}
+if(100 > temp > 70) {
+    console.log("Good day for activities");
+}
+if( temp >= 100) {
+    console.log("heat wave");
+}
+
+}
+weather(170);
