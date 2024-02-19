b900d9a42fcf450be5881a05cebc7f240dad1e11
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-19 13:05:05 +0000

    jh

---
+++ array.js
@@ -0,0 +1,11 @@
+var teams = [
+    ["Barca", 10],
+    ["madrid", 9],
+    ["chelsea", 9],
+    ["city", 8],
+];
+
+console.log(teams[1][0]);
+console.log(teams[0][1]);
+console.log(teams[3]);
+    
