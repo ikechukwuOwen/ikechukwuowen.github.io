772be2c886b603939ae679d57dc73a19424751be
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-19 13:31:59 +0000

    f

--- array.js
+++ array.js
@@ -9,11 +9,15 @@ console.log(teams[1][0]);
 console.log(teams[0][1]);
 console.log(teams[3]);
 
+    
+teams.push("liver", 8);
+console.log(teams[4]);
+
 var a = teams.toString();
 console.log(a);
 
+
 var b = teams.length;
 console.log(b);
-    
-teams.push("liver");
-console.log(teams[4]);
+
+
