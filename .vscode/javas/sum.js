334e423af278896c2c49b3db168f903c9c0b63e9
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-20 12:25:53 +0000

    bb

---
+++ .vscode/javas/sum.js
@@ -0,0 +1,13 @@
+var sim = [ 20, 39, 50];
+var arraySum = function(array){
+    var sum = 0;
+    for( i = 0; i < array.length; i++){
+      sum = sum + array[i];
+    }
+     return sum;
+  }
+
+
+ var u = arraySum([30, 40, 30]);
+ console.log( u);
+  
