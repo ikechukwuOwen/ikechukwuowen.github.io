7f2dbf48d116a459505bdfbb78e3dfa14478f9b4
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-29 15:19:47 +0000

    fh

--- 2object.js
+++ 2object.js
@@ -19,7 +19,41 @@ var scores = [
 }
 ]
 
-for ( print in scores[0]){
-  console.log(print);
-  console.log (scores[0][print]);
+var rate = function (num ){
+var bat = 0;
+var mat = 0;
+var eat = 0;
+var oat = 0;
+  for( a = 0; a < scores.length; a++){
+    for ( print in scores[a]){
+      console.log(print);
+      console.log (scores[a][print]);
+    }
+    bat = scores[a]["grade"];
+    mat = scores [a]["grade"]["Maths"];
+    eat = scores [a]["grade"]["English"];
+    oat = scores [a]["grade"]["Others"];
+      if ( mat >= 70 ){
+        console.log(scores[a]["name"], "passed", "Maths");
+      }
+      else { 
+        console.log(scores[a]["name"], "failed", "Maths");
+      }
+      if ( eat >= 70 ){
+        console.log(scores[a]["name"], "passed", "English");
+      }
+      else { 
+        console.log(scores[a]["name"], "failed", "English");
+    }
+    if ( oat >= 70 ){
+      console.log(scores[a]["name"], "passed", "others");
+    }
+    else { 
+      console.log(scores[a]["name"], "failed", "others");
+  }
+
 }
+}
+
+var ben = rate(scores);
+console.log(ben);
