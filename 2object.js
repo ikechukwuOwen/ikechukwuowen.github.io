ecf7f684ab362934991b577396784ee0cd2ae8f9
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-02-29 14:01:27 +0000

    jd

--- 2object.js
+++ 2object.js
@@ -1,11 +1,25 @@
-var scores = {
+var scores = [
+ {
     name: "ikechukwu owen",
     age : 19,
     grade: {
-      Maths : [99],
-      English: [90],
-      Others : [89],
+      Maths : 99,
+      English: 90,
+      Others : 89,
     }
+},
+  {
+  name: "Micheal Bowen",
+  age : 29,
+  grade: {
+    Maths : 49,
+    English: 90,
+    Others : 39,
+  }
 }
+]
 
-console.log(scores.name);
+for ( print in scores[0]){
+  console.log(print);
+  console.log (scores[0][print]);
+}
