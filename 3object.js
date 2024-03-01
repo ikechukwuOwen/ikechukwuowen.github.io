41d1d6024921807be94084d5f065f150b2a6a4f8
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-03-01 17:20:29 +0000

    hh

---
+++ 3object.js
@@ -0,0 +1,22 @@
+var scores = [
+    {
+       name: "ikechukwu owen",
+       age : 19,
+       grade: {
+         Maths : 99,
+         English: 90,
+         Others : 89,
+       }
+   },
+     {
+     name: "Micheal Bowen",
+     age : 29,
+     grade: {
+       Maths : 49,
+       English: 90,
+       Others : 39,
+     }
+   }
+]
+
+console.log(scores[0]);
