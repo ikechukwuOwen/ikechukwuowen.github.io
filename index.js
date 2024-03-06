131ef0d394cad2a154002f8f4773f99b046c5878
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-03-06 15:46:04 +0000

    Update index.html and index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var h1 = document.querySelector('h1');
 h1.innerHTML = "NOLLYWOOD CENTRE MOVIES";
+
+var thanks = document.getElementById('thanks');
+thanks.innerHTML = "Thanks for visiting Nollywood Centre tv";
