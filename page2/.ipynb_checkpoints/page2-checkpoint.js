0c4dbf531c6df04d5a0695c0278e1f5324fc83bc
ikechukwuOwen <155768293+ikechukwuOwen@users.noreply.github.com>
2024-03-28 20:33:40 +0000

    hhh

---
+++ page2/.ipynb_checkpoints/page2-checkpoint.js
@@ -0,0 +1,16 @@
+var h1 = document.querySelector('h1');
+    h1.innerHTML = "Information Page";
+
+var foot = document.getElementById('footer');
+for (var i = 0; i < foot.children.length; i++) {
+    foot.children[i].style.color = "red";
+}
+
+var nee = document.createElement('p');
+nee.innerHTML = "Supervised by: Dr. ik";
+
+var note = document.getElementsByClassName('note')[0];
+note.appendChild(nee);
+
+
+
