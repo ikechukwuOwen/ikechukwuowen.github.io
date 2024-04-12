6ca26ac2f0201eb479c2cafa03ed54c1fd3fac05
ikechukwuOwen <ezunuowen@gmail.com>
2024-04-12 14:24:47 +0100

    h3bb

--- list/list.js
+++ list/list.js
@@ -2,11 +2,8 @@
  var list = document.getElementById('ollist');
 
     add.onclick = function() {
-        if (document.getElementById('newmovie').value == '') {
-            alert('Please enter a movie name');
-            return;
-        }
         var put = document.getElementById('newmovie').value;
+        
         var li = document.createElement('li');
         li.innerHTML = put;
         list.appendChild(li);
