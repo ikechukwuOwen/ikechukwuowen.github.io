f45ed5bead6253ba641b33e44c6eebc1e6770084
ikechukwuOwen <ezunuowen@gmail.com>
2024-04-20 11:22:12 +0100

    mm

--- list/list.js
+++ list/list.js
@@ -1,19 +1,15 @@
  var add = document.getElementById('sub');
-var list = document.getElementById('list');
+ var list = document.getElementById('list');
 
-add.onclick = function() {
-    var put = document.getElementById('newMovie').value;
-    
-    if (put !== '') {
-        
-    var li = document.createElement('li');
-    li.innerHTML = put;
-    list.appendChild(li);
-    document.getElementById('newMovie').value = '';
-    }
+ add.onclick = function() {
+     var put = document.getElementById('newMovie').value;
 
-}
+     if (put !== '') {
 
+         var li = document.createElement('li');
+         li.innerHTML = put;
+         list.appendChild(li);
+         document.getElementById('newMovie').value = '';
+     }
 
-
-    
+ }
