155767132ff6d940e41d4841454be7661391d2a3
ikechukwuOwen <ezunuowen@gmail.com>
2024-05-03 12:08:13 +0100

    hh

--- list/list.js
+++ list/list.js
@@ -13,3 +13,13 @@
      }
 
  }
+ var images = [
+     "/media/vlcsnap-2023-02-11-13h30m53s540.png",
+     "/media/vlcsnap-2023-02-11-13h33m04s529.png",
+     "/media/vlcsnap-2023-07-30-14h39m56s452.png",
+     "/media/vlcsnap-2023-07-30-14h48m50s084.png",
+     "/media/vlcsnap-2023-08-19-00h27m50s446.png",
+     "/media/vlcsnap-2023-08-31-18h11m52s547.png",
+     "/media/vlcsnap-2024-01-16-14h11m33s038.png",
+ ];
+ var currentIndex = 0;
