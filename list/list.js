4f8d83d2c59bf2c07316ac4be3186cc52897d860
ikechukwuOwen <ezunuowen@gmail.com>
2024-05-13 12:11:33 +0100

    bb

--- list/list.js
+++ list/list.js
@@ -20,6 +20,16 @@
      "/media/vlcsnap-2023-07-30-14h48m50s084.png",
      "/media/vlcsnap-2023-08-19-00h27m50s446.png",
      "/media/vlcsnap-2023-08-31-18h11m52s547.png",
-     "/media/vlcsnap-2024-01-16-14h11m33s038.png",
+     "/media/vlcsnap-2024-01-16-14h11m33s038.png"
  ];
  var currentIndex = 0;
+ var incrementIndex = function() {
+     currentIndex = currentIndex + 1;
+     if (currentIndex > images.length - 1) { currentIndex = 0 }
+     return currentIndex;
+ }
+ var img = document.querySelector('img');
+ var next = document.getElementById('next');
+ next.onclick = function() {
+     img.setAttribute('src', images[incrementIndex(currentIndex)]);
+ }
